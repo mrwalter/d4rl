00512b41b8d4508e02dc67c608cd1ee9fd967b49
Matthew Walter <mwalter@ttic.edu>
2022-01-09 07:43:13 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control'
+                      'dm_control',
                       #'dm_control' if 'macOS' in platform() else
                       #'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
