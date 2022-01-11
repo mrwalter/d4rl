dfb2dcdbc7ef730abd24cc742dc0fb0989de9ec7
Matthew R. Walter <mwalter@ttic.edu>
2022-01-10 18:14:10 -0600

    changed mjrl dependency

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
                       'dm_control',
                       #'dm_control' if 'macOS' in platform() else
                       #'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
