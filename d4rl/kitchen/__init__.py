dccdf156230953fa75f6aeee115764a675825b89
Matthew R. Walter <mwalter@ttic.edu>
2022-01-12 09:17:17 -0600

    removed redundant entry

--- d4rl/kitchen/__init__.py
+++ d4rl/kitchen/__init__.py
@@ -72,14 +72,3 @@ register(
         'dataset_url': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/kitchen/kitchen_microwave_kettle_bottomburner_light-v0.hdf5'
     }
 )
-
-register(
-    id='kitchen-microwave-kettle-topburner-light-v0',
-    entry_point='d4rl.kitchen:KitchenMicrowaveKettleTopburnerLightV0',
-    max_episode_steps=280,
-    kwargs={
-        'ref_min_score': 0.0,
-        'ref_max_score': 4.0,
-        'dataset_url': 'http://rail.eecs.berkeley.edu/datasets/offline_rl/kitchen/kitchen_microwave_kettle_bottomburner_light-v0.hdf5'
-    }
-)
